64a72123fd1f60a65922033de4ad694f3e2608e5
robin <robin@kazaden.com>
2018-11-07 14:34:27 +0100

    ready for release

--- bien_evidemment.py
+++ bien_evidemment.py
@@ -4,12 +4,16 @@ from mastodon import Mastodon
 import os
 import random
 from datetime import datetime
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
 # read config from file
-config =
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
 
-# get token from env variable
-token = os.environ['MASTODON_BEARER_TOKEN']
+# get token from env variable (from .env)
+token = os.getenv('MASTODON_BEARER_TOKEN')
 
 # get API client
 mastodon = Mastodon(access_token = token, api_base_url = config['base_url'])
@@ -20,14 +24,15 @@ def get_random_answer():
   answers = [
     "Bien evidemment !",
     "BIEN EVIDEMMENT !",
-    "BIEN EVIDEMM{0}NT !!".format("N" * random.randint(1, 10))
+    "BIEN EVIDEMM{0}NT !!".format("E" * random.randint(1, 10))
   ]
   return random.choice(answers)
 
 
 # get all mentions of the bot (@-ed)
 notifications = mastodon.notifications()
-mentions = filter(lambda n: n['type'] == 'mention')
+mentions = list(filter(lambda n: n['type'] == 'mention', notifications))
+print("{0}: {1} 🔔".format(datetime.now(), len(mentions)))
 
 # answer to all mentions
 for mention in mentions:
@@ -35,13 +40,13 @@ for mention in mentions:
   ranswer = get_random_answer()
 
   # posting in reply to mention
-  print("{0}: 👤 {1}".format(datetime.now, mention['account']['username']))
-  print("{0}: 📯 {1}".format(datetime.now, ranswer))
-  mastodon.status_post(ranswer, in_reply_to_id=mention['status'] spoiler_text="bien evidemment")
-  print("{0}:  👍".format(datetime.now))
+  print("{0}: 👤 {1}".format(datetime.now(), mention['account']['username']))
+  print("{0}: 📯 {1}".format(datetime.now(), ranswer))
+  mastodon.status_reply(to_status=mention['status'], status=ranswer, spoiler_text="bien evidemment", visibility='public')
+  print("{0}: 👍".format(datetime.now()))
 
   # dismiss this notification, so it doesn't appear
   # when fetching notifications later
   mastodon.notifications_dismiss(mention)
 
-print("{0}:  👍 😴".format(datetime.now))
+print("{0}: 👍 😴".format(datetime.now()))
